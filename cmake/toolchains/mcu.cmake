5dd009db2e94c0fee71fff2a08c6114d39b769de
HVMech <hvpmachine@gmail.com>
2025-11-30 09:54:56 +0300

    Added ctest interface

--- cmake/toolchains/mcu.cmake
+++ cmake/toolchains/mcu.cmake
@@ -12,6 +12,13 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 # TODO: experiment with flags
 # set(COMMON_FLAGS "-mcpu=cortex-m3 -mthumb -specs=nosys.specs -ffunction-sections -fdata-sections")
 set(COMMON_FLAGS "-mcpu=cortex-m3 -O2 -mthumb -specs=nosys.specs")
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(COMMON_FLAGS "${COMMON_FLAGS} -DDEBUG")
+else()
+    set(COMMON_FLAGS "${COMMON_FLAGS}")
+endif()
+
 #set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu11")
 #set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=gnu++17 -fno-rtti -fno-exceptions")
 set(CMAKE_CXX_FLAGS "${COMMON_FLAGS}")
@@ -19,6 +26,7 @@ set(CMAKE_ASM_FLAGS "-mcpu=cortex-m3 -mthumb")
 # set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -Wl,--gc-sections")
 set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles")
 
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 message(STATUS "CXX Flags: ${CMAKE_CXX_FLAGS}")
 message(STATUS "ASM Flags: ${CMAKE_ASM_FLAGS}") 
 message(STATUS "Linker Flags: ${CMAKE_EXE_LINKER_FLAGS}")
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 1. mkdir build
 2. cd build
-3. 1. cmake .. -DPLATFORM=host -DUSE_MODERN_CPP=ON
-3. 2. cmake .. -DPLATFORM=mcu -DUSE_MODERN_CPP=ON
+3. 1. cmake ..
+3. 1. 1. -DPLATFORM=mcu или host
+3. 1. 2. -DCMAKE_BUILD_TYPE=Debug или Release
+3. 1. 3. -DUSE_MODERN_CPP=ON или OFF
 4. cmake --build .
 5. 1. openocd -f interface/stlink-v2.cfg -f target/stm32f1x.cfg -c "program platforms/mcu/bare/SMCP.elf verify reset exit"
-5. 2. ./platforms/host/run_all_tests
+5. 2. ctest --test-dir ./platforms/host -V
